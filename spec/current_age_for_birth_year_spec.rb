1caf3155b896e3e161fa0f9f1b301b7846ea62b9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-22 22:03:49 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,8 +2,9 @@ require_relative '../current_age_for_birth_year.rb'
 #loads code from program file
 
 describe "current_age_for_birth_year method" do
-  #describe is RSpec Method 
+  #describe is RSpec Method
   it "returns the age of a person based on the year of birth" do
+    #it is RSpec Method to describe behavior of method
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
